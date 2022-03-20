ecf5e968b0ac4304f4d5dc725bc6417a6b49a24d
Documenter.jl <documenter@juliadocs.github.io>
2022-03-20 09:48:01 +0000

    build based on 7bf0a17

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FinancialAssets","category":"page"},{"location":"#FinancialAssets","page":"Home","title":"FinancialAssets","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FinancialAssets.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FinancialAssets]","category":"page"}]
+}
